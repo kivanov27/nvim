62549e7d6463f16bec1340d76cf9d94bc74caca8
fdd <kristiyan.ivanov.98@gmail.com>
2026-02-26 13:03:24 +0200

    reduced neoscroll delay from 250 to 150ms

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -8,12 +8,12 @@ return {
             local neoscroll = require("neoscroll")
 
             vim.keymap.set("n", "<C-d>", function()
-                neoscroll.ctrl_d({ duration = 250 })
+                neoscroll.ctrl_d({ duration = 150 })
                 vim.cmd("normal! zz")
             end)
 
             vim.keymap.set("n", "<C-u>", function()
-                neoscroll.ctrl_u({ duration = 250 })
+                neoscroll.ctrl_u({ duration = 150 })
                 vim.cmd("normal! zz")
             end)
         end,
