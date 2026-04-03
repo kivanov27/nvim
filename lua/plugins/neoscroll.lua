1baaf6741a5beb53d9cd77d3cbd3bdecdca5c058
fdd <kristiyan.ivanov.98@gmail.com>
2026-04-03 10:24:29 +0300

    Lowered Neoscroll animation from 150ms to 100ms.

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -8,12 +8,12 @@ return {
         neoscroll.setup(opts)
 
         vim.keymap.set("n", "<C-d>", function()
-            neoscroll.ctrl_d({ duration = 150 })
+            neoscroll.ctrl_d({ duration = 100 })
             vim.cmd("normal! zz")
         end)
 
         vim.keymap.set("n", "<C-u>", function()
-            neoscroll.ctrl_u({ duration = 150 })
+            neoscroll.ctrl_u({ duration = 100 })
             vim.cmd("normal! zz")
         end)
     end,
