1a16bd379f8bb17b7949cce13112bfa884b79b4e
fdd <kristiyan.ivanov.98@gmail.com>
2026-05-24 12:26:21 +0300

    Disabled mini.ai and neoscroll

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,7 +4,7 @@ return {
         --  - va)  - [V]isually select [A]round [)]paren
         --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
         --  - ci'  - [C]hange [I]nside [']quote
-        require("mini.ai").setup({ n_lines = 500 })
+        -- require("mini.ai").setup({ n_lines = 500 })
         -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
         -- - sd'   - [S]urround [D]elete [']quotes
         -- - sr)'  - [S]urround [R]eplace [)] [']
